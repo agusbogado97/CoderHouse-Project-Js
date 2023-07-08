e0faa9667aa3ab3f244a6acf9850b30cb48879b8
Agustín Bogado <agus.y.roger@gmail.com>
2023-07-08 11:06:08 -0300

    Actualización Booleans

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-let nombreDeAlumnos = prompt("Ingrese el nombre del alumno para saber su nota: ")
-const notaAprobada = 5
-const notaRegular = 3
-const notaDesaprobada = 1
+let nombreDeAlumnos = prompt("Ingrese el nombre del alumno para saber su nota: ");
+const notaAprobada = 5;
+const notaRegular = 3;
+const notaDesaprobada = 1;
 
-function mostrarNota(nombreDeAlumnos){
-    const nombreAgustin = "agustin"
-    const nombreMarcos = "marcos"
-    const nombreRobert = "robert"
+function mostrarNota(nombreDeAlumnos) {
+    const nombreAgustin = "agustin";
+    const nombreMarcos = "marcos";
+    const nombreRobert = "robert";
 
-    if(nombreDeAlumnos == nombreAgustin){
-        return "La nota es: " + notaRegular
-    } else if(nombreDeAlumnos == nombreMarcos){
-        return "La nota es: " + notaDesaprobada
-    } else if(nombreDeAlumnos == nombreRobert){
-        return "La nota es: " + notaAprobada
-    } else;{
-        return "¿Quién te conoce papá"
+    nombreDeAlumnos = nombreDeAlumnos.toLowerCase();
+
+    if (nombreDeAlumnos == nombreAgustin) {
+        return {
+            nota: notaRegular,
+            aprobado: false
+        };
+    } else if (nombreDeAlumnos == nombreMarcos) {
+        return {
+            nota: notaAprobada,
+            aprobado: notaAprobada == notaAprobada
+        };
+    } else if (nombreDeAlumnos == nombreRobert) {
+        return {
+            nota: notaDesaprobada,
+            aprobado: false
+        };
+    } else {
+        console.log("¿Quién te conoce papá?");
+        return null;
     }
 }
 
+const resultado = mostrarNota(nombreDeAlumnos);
 
-console.log(mostrarNota(nombreDeAlumnos))
+if (resultado !== null) {
+    console.log("La nota es:", resultado.nota);
+    console.log("¿Aprobado?", resultado.aprobado);
+}
