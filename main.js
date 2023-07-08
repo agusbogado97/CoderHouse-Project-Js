c838331c9ac59758756347c931ffa7b1cfe7b320
Agustín Bogado <agus.y.roger@gmail.com>
2023-07-08 11:22:30 -0300

    Agregando ciclo for

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-let nombreDeAlumnos = prompt("Ingrese el nombre del alumno para saber su nota: ");
+const cantidadAlumnos = 3;
 const notaAprobada = 5;
 const notaRegular = 3;
 const notaDesaprobada = 1;
 
-function mostrarNota(nombreDeAlumnos) {
-    const nombreAgustin = "agustin";
-    const nombreMarcos = "marcos";
-    const nombreRobert = "robert";
+for (let i = 1; i < cantidadAlumnos; i++) {
+    let nombreDeAlumnos = prompt("Ingrese el nombre del alumno para saber su nota: ");
 
-    nombreDeAlumnos = nombreDeAlumnos.toLowerCase();
+    function mostrarNota(nombreDeAlumnos) {
+        const nombreAgustin = "agustin";
+        const nombreMarcos = "marcos";
+        const nombreRobert = "robert";
 
-    if (nombreDeAlumnos == nombreAgustin) {
-        return {
-            nota: notaRegular,
-            aprobado: false
-        };
-    } else if (nombreDeAlumnos == nombreMarcos) {
-        return {
-            nota: notaAprobada,
-            aprobado: notaAprobada == notaAprobada
-        };
-    } else if (nombreDeAlumnos == nombreRobert) {
-        return {
-            nota: notaDesaprobada,
-            aprobado: false
-        };
-    } else {
-        console.log("¿Quién te conoce papá?");
-        return null;
+        nombreDeAlumnos = nombreDeAlumnos.toLowerCase();
+
+        if (nombreDeAlumnos == nombreAgustin) {
+            return {
+                nota: notaRegular,
+                aprobado: false
+            };
+        } else if (nombreDeAlumnos == nombreMarcos) {
+            return {
+                nota: notaAprobada,
+                aprobado: notaAprobada == notaAprobada
+            };
+        } else if (nombreDeAlumnos == nombreRobert) {
+            return {
+                nota: notaDesaprobada,
+                aprobado: false
+            };
+        } else {
+            console.log("¿Quién te conoce papá?");
+            return null;
+        }
     }
-}
 
-const resultado = mostrarNota(nombreDeAlumnos);
+    const resultado = mostrarNota(nombreDeAlumnos);
 
-if (resultado !== null) {
-    console.log("La nota es:", resultado.nota);
-    console.log("¿Aprobado?", resultado.aprobado);
+    if (resultado !== null) {
+        console.log("La nota es:", resultado.nota);
+        console.log("¿Aprobado?", resultado.aprobado);
+    }
 }
